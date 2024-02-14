ea3ce095dc8a0752fb2fd574aa5c869fbaff5de9
Akshay Patra <akshay.patra114@gmail.com>
2024-02-14 14:15:39 +0530

    added responsiveness and completed the about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,8 +2,27 @@ import React from 'react'
 
 export default function AboutPage() {
   return (
-    <div>
-       This is about page
-    </div>
+    <section className='AboutPageComponent'>
+
+
+        <div className='about-myself'>
+          <h1>About</h1>
+          <p >
+            I'm a React developer based in India . I'm currently pursuing Btech degree in Computer Sceince and Engineering <br/>
+            and actively developing my expertise as a React Developer. I have completed my schooling from Kendriya Vidyalaya <br/>
+            achieving the title of top scorer in Computer Sceince with 100% result in CBSE boards.<br/>
+            I'm actively participating in bootcamps and club activities  hosted by the college honing my skills in various Domains.  
+          </p>
+        </div>
+        <div className='skills'>
+          <h1>Knows </h1>
+          <p>
+           <span>Web technology :</span> HTML, CSS, Javascript, Bootstrap, React JS<br/>
+           <span>Programing Language :</span> Javascript, Python, C++, Java<br/>
+           <span>Database : </span>MongoDB, MySQL
+          </p>
+        </div>
+
+    </section>
   )
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,26 +1,27 @@
 import React from 'react'
+import logo from '../IMG_20200928_102321.jpg';
+
 
 export default function HomePage() {
   return (
     <section className='homepage'>
-        <div className='Column-1'>
+        
            <div className='description'>
+            <div>
                 <h1>Hello , I am Akshay Patra</h1>
                 <p className='p1'> I am a front-end web developer,<br/>
                      Exploring the intersection of code and creativity  and  <br>
                 </br>dedicated to push the boundaries of innovation in the digital realm
                 </p>
-                <h3>Knows </h3> 
-                <p className='p2'> 
-                    Web Technologies : HTMl , CSS , React.js ,Bootstrap<br/>
-                    Programming Languages : Python , C++ , Javascript <br/>
-
-                </p>
+                </div>   
+               
            </div>
-        </div>
-        <div className='Column-2'>
-            <div className='picture'> </div><br/>
-        </div>
+
+           <div className='display-picture'>
+            <img src={logo} alt='user photo'/>
+          </div>
+        
+       
     </section>
   )
 }
