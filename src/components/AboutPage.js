e9e671c90c4b8902438e76d1cdb83b159611744b
Akshay Patra <akshay.patra114@gmail.com>
2024-02-18 21:27:53 +0530

    corrected some mistakes

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -10,7 +10,7 @@ export default function AboutPage() {
           <p >
             I'm a React developer based in India . I'm currently pursuing Btech degree in Computer Sceince and Engineering <br/>
             and actively developing my expertise as a React Developer. I have completed my schooling from Kendriya Vidyalaya <br/>
-            achieving the title of top scorer in Computer Sceince with 100% result in CBSE boards.<br/>
+            achieving the title of top scorer in Computer Science with 100% result in CBSE boards.<br/>
             I'm actively participating in bootcamps and club activities  hosted by the college honing my skills in various Domains.  
           </p>
         </div>
